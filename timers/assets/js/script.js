7608bcb502a9917e1acdf9aec1525a68341de389
Luan <luanleite202@gmail.com>
2024-08-21 19:48:08 -0300

    update after error clock is not defined

--- timers/assets/js/script.js
+++ timers/assets/js/script.js
@@ -1,52 +1,54 @@
 
-
-const getTimeSeconds = (seconds) => {
-    const data = new Date(seconds * 1000);
-    return data.toLocaleTimeString("pt-Br", {
-        hour12: false,
-        timeZone: "UTC"
-    });
-}
-
-
-let seconds = 0;
-let timer;
-
-const startClock = () => {
-    timer = setInterval(() => {
-        seconds++;
-        clock.innerHTML = getTimeSeconds(seconds);
-    }, 1000)
-    console.log("ok")
-}
-
-document.addEventListener("click", (event) => {
-    const el = event.target;
-
-    if (el.classList.contains("reset")){
-        console.log("resetei");
-        clearInterval(timer)
-        clock.innerHTML = "00:00:00";
-        seconds = 0;
-        clock.classList.remove("stop")
-
+const clockProject = () => {
+    const getTimeSeconds = (seconds) => {
+        const data = new Date(seconds * 1000);
+        return data.toLocaleTimeString("pt-Br", {
+            hour12: false,
+            timeZone: "UTC"
+        });
     }
 
-    if (el.classList.contains("start")){
-        clearInterval(timer);
-        console.log("startei");
-        clock.classList.remove("stop")
-        startClock();
+    const clock = document.querySelector(".timer");
+
+    let seconds = 0;
+    let timer;
+
+    const startClock = () => {
+        timer = setInterval(() => {
+            seconds++;
+            clock.innerHTML = getTimeSeconds(seconds);
+        }, 1000)
+        console.log("ok")
     }
 
-    if (el.classList.contains("pause")){
-        console.log("pausei");
-        pauseClock(timer);
-        clock.classList.add("stop")
-    }
-})
+    document.addEventListener("click", (event) => {
+        const el = event.target;
 
-const pauseClock = (timer) => {
-    return clearInterval(timer);
+        if (el.classList.contains("reset")){
+            console.log("resetei");
+            clock.innerHTML = "00:00:00";
+            pauseClock(timer);
+            clock.classList.remove("stop")
+            seconds = 0;
+        }
+
+        if (el.classList.contains("start")){
+            clearInterval(timer);
+            console.log("startei");
+            clock.classList.remove("stop")
+            startClock();
+        }
+
+        if (el.classList.contains("pause")){
+            console.log("pausei");
+            pauseClock(timer);
+            clock.classList.add("stop")
+        }
+    })
+
+    const pauseClock = (timer) => {
+        return clearInterval(timer);
+    }
 }
 
+clockProject();
