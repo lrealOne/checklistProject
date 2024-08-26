d0222cd2fcb95f0d0fec7fcb362cba984b5c6570
Luan <luanleite202@gmail.com>
2024-08-25 21:38:52 -0300

    Fisish other project (OBS: chatgpt made css code)

--- checklist/assets/js/checklist.js
+++ checklist/assets/js/checklist.js
@@ -2,7 +2,7 @@ const checkList = () => {
 
     const addTarefa = document.querySelector(".addNovaTarefa")
     const btnAdd = document.querySelector(".btnEnviar")
-    const toDoList = document.querySelector(".lista")
+    const lista = document.querySelector(".lista")
 
 
     //adicionando evento quando clico no spacebar
@@ -23,9 +23,11 @@ const checkList = () => {
     const criaTarefa = (texto) => {
         const li = criaLi();
         li.innerHTML = texto;
-        toDoList.appendChild(li);
+        li.setAttribute("class", "tarefa")
+        lista.appendChild(li);
         clearInput();
-        newBtn(li)
+        newBtn(li);
+        saveList();
     }
 
     // função para limpar o input sempre que adicionar um item
@@ -43,14 +45,61 @@ const checkList = () => {
     // função que cria um novo botão para cada item adicionado
     const newBtn = (li) => {
         li.innerHTML += ""
-        const btn = document.createElement("button");
-        btn.innerText = "Apagar";
-        btn.setAttribute("class", "delete")
-        btn.setAttribute("title", "Apagar tarefa")
-        li.appendChild(btn);
+        const btn1 = document.createElement("button");
+        btn1.innerText = " Apagar";
+        btn1.setAttribute("class", "delete")
+        btn1.setAttribute("title", "Apagar tarefa")
+
+        const btn2 = document.createElement("button");
+        btn2.innerText = "Riscar";
+        btn2.setAttribute("class", "st")
+        btn2.setAttribute("title", " Riscar tarefa")
+        li.appendChild(btn1);
+        li.appendChild(btn2)
     }
 
+    document.addEventListener("click", (event) => {
+        const element_ = event.target;
+        
+        if (element_.classList.contains("delete")){
+            element_.parentElement.remove();
+        }
+        saveList()
+    })
 
+    document.addEventListener("click", (event) => {
+        const el = event.target;
+
+        if (el.classList.contains("st")){
+            el.parentElement.classList.add("strike");
+        }
+        saveList()
+    })
+
+    const saveList = () => {
+        const listLi = lista.querySelectorAll("li")
+        const toDoList = [];
+
+        for (let list of listLi){;
+            let tarefaText = list.innerText;
+            tarefaText = tarefaText.replace("Apagar", "").trim()
+            tarefaText = tarefaText.replace("Riscar", "").trim()
+            toDoList.push(tarefaText)
+        }
+        
+        const tarefasJSON = JSON.stringify(toDoList)
+        localStorage.setItem("tarefas", tarefasJSON)
+    }
+
+    const readSaveList = () => {
+    const tarefas = localStorage.getItem("tarefas")
+    const turnJSON = JSON.parse(tarefas)
+
+    for (tarefa of turnJSON) {
+            criaTarefa(tarefa)
+        }
+    }
+    readSaveList()
 }
 
-checkList();
+checkList()
