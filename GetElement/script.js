789a6b44b493e67ca2c133f1cd852722102fef9f
Luan Leite <luanleite202@gmail.com>
2024-08-25 21:45:04 -0300

    Delete GetElement directory

--- GetElement/script.js
+++
@@ -1,8 +0,0 @@
-console.log("sexo")
-const number = prompt("Digite um numero: ");
-let userNum = document.getElementById("userNum");
-
-userNum.innerText = number;    
-
-let squareRoot = document.getElementById("squareRoot");
-squareRoot.innerText= number ** (1/2);
